9db6a0839a700a346a4fa8d36c6f79f22688634d
Sveta_Burakova <svetlaburs@gmail.com>
2023-03-27 18:05:59 +0300

    Загружает превью с сервера

--- js/create-previews.js
+++ js/create-previews.js
@@ -1,14 +1,16 @@
-import {listDesc} from './desc-generator.js';
-
 const picturesList = document.querySelector('.pictures');
 const pictureTemplate = document.querySelector('#picture').content.querySelector('.picture');
 
-listDesc.forEach(({id, likes, url, description, commentsList}) => {
-  const pictureElement = pictureTemplate.cloneNode(true);
-  picturesList.appendChild(pictureElement);
-  pictureElement.querySelector('.picture__likes').textContent = likes;
-  pictureElement.querySelector('.picture__img').src = url;
-  pictureElement.querySelector('.picture__img').alt = description;
-  pictureElement.dataset.id = id;
-  pictureElement.querySelector('.picture__comments').textContent = commentsList.length;
-});
+const createPreviews = (data) => {
+  data.forEach(({id, likes, url, description, comments}) => {
+    const pictureElement = pictureTemplate.cloneNode(true);
+    picturesList.appendChild(pictureElement);
+    pictureElement.querySelector('.picture__likes').textContent = likes;
+    pictureElement.querySelector('.picture__img').src = url;
+    pictureElement.querySelector('.picture__img').alt = description;
+    pictureElement.dataset.id = id;
+    pictureElement.querySelector('.picture__comments').textContent = comments.length;
+  });
+};
+
+export{createPreviews};
--- js/main.js
+++ js/main.js
@@ -1,5 +1,11 @@
-import './create-previews.js';
+import { createPreviews } from './create-previews.js';
 import './full-view.js';
 import './upload-form.js';
 import './effects.js';
 import './scale.js';
+
+fetch('https://28.javascript.pages.academy/kekstagram/data')
+  .then((Response)=> Response.json())
+  .then((pictures) => {
+    createPreviews(pictures);
+  });
